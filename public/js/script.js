f398e5f306edf1508c99c8dfffbae69553cc6009
Nathanael Jones <nathanael.jones@gmail.com>
2011-04-27 13:37:41 +0200

    Fixed scripts so they load in subdirectories. Fixed code font size for outside the home page. Fixed configuration page. Added managed API page.

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,11 @@
-$(function(){
-	$(".banners").jshowoff({speed:10000});
-});
 hljs.tabReplace = '  '; 
 hljs.initHighlightingOnLoad();
+
+$(function(){
+	$(".banners").jshowoff({speed:10000});
+	/*$('pre code').not('code[class]').each(function(i, e) {$(e).addClass('csharp c-sharp'); hljs.highlightBlock(e, '  ')});*/
+});
+
 $('a[href$=".zip"]').click(function(){
 	_gaq.push(['_trackPageview', $(this).attr('href') ]);
 });
