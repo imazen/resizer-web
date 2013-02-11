7cb93bb29580e076d2f0136671472a11310c5432
Nathanael Jones <nathanael.jones@gmail.com>
2013-02-11 14:36:51 +0100

    Fixed nullref in script.js (hljs customization)

--- content/js/script.js
+++ content/js/script.js
@@ -7,10 +7,10 @@ window.log = function(){
 (function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,markTimeline,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();)b[a]=b[a]||c})(window.console=window.console||{});
 
 
-
+if (hljs){
 hljs.tabReplace = '  '; 
 hljs.initHighlightingOnLoad();
-
+}
 $(function(){
 	if (!$(".banners").bxSlider) return;
 	$(".banners").bxSlider({
