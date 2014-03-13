62f649de5761310635c850fb82a31f07ee0c2b3f
Nathanael Jones <nathanael.jones@gmail.com>
2014-03-13 17:48:38 -0400

    Add textillate library

--- content/js/custom.js
+++ content/js/custom.js
@@ -25,6 +25,85 @@ jQuery(document).ready(function($){
 			namespace: "mr-rotato" // Defaults to “carousel”.
 	})
 
+  if ($.textillate){
+    $('.textillate').textillate({
+
+      initialDelay: 0,
+      in: {
+        delay: 10
+      },
+      out: {
+        effect: 'fade',
+        delay: 5
+      }
+    });
+  }
+/*
+  selector: '.texts',
+
+  // enable looping
+  loop: false,
+
+  // sets the minimum display time for each text before it is replaced
+  minDisplayTime: 2000,
+
+  // sets the initial delay before starting the animation
+  // (note that depending on the in effect you may need to manually apply 
+  // visibility: hidden to the element before running this plugin)
+  initialDelay: 0,
+
+  // set whether or not to automatically start animating
+  autoStart: true,
+
+  // custom set of 'in' effects. This effects whether or not the 
+  // character is shown/hidden before or after an animation  
+  inEffects: [],
+
+  // custom set of 'out' effects
+  outEffects: [ 'hinge' ],
+
+  // in animation settings
+  in: {
+    // set the effect name
+    effect: 'fadeInLeftBig',
+
+    // set the delay factor applied to each consecutive character
+    delayScale: 1.5,
+
+    // set the delay between each character
+    delay: 50,
+
+    // set to true to animate all the characters at the same time
+    sync: false,
+
+    // randomize the character sequence 
+    // (note that shuffle doesn't make sense with sync = true)
+    shuffle: false,
+
+    // reverse the character sequence 
+    // (note that reverse doesn't make sense with sync = true)
+    reverse: false,
+
+    // callback that executes once the animation has finished
+    callback: function () {}
+  },
+
+  // out animation settings.
+  out: {
+    effect: 'hinge',
+    delayScale: 1.5,
+    delay: 50,
+    sync: false,
+    shuffle: false,
+    reverse: false,
+    callback: function () {}
+  },
+
+  // callback that executes once textillate has finished 
+  callback: function () {}
+});
+*/
+
 });
 
 
