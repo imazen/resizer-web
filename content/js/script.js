a2fbe219ae8c41037e346a651983217b7a41399f
Nathanael Jones <nathanael.jones@gmail.com>
2014-03-27 17:06:11 -0400

    Add SourceCodePro font to all pages. Add prism lib, add typed.js usage code.

--- content/js/script.js
+++ content/js/script.js
@@ -10,7 +10,7 @@ window.slimmage = window.slimmage | {};
 window.slimmage.verbose = false;
 
 
-if (hljs){
+if (window.hljs !== undefined){
 hljs.tabReplace = '  '; 
 hljs.initHighlightingOnLoad();
 }
@@ -124,6 +124,34 @@ $(function(){
   },1500);
   $(".ml_noun").text()
 });
+
+
+  $(function(){
+      var queryStrings = ["height=150", 
+                          "w=170&h=170&mode=crop",
+                          "w=170&h=170&mode=pad",
+                          "w=170&h=170&mode=max",
+                          "h=200&s.grayscale=true",
+                          "crop=770,590,1170,790",
+                          "srotate=90&height=200",
+                          "h=200&s.sepia=true"
+                          ];
+      var img = $('.change-this-image');
+      var imgPath = "http://z.zr.io/ri/u3.jpg?"
+      $(".home-code-box .type-this").typed({
+        strings: queryStrings,
+        backDelay:2000,
+        loop:true,
+        typeSpeed: 50,
+        callback: function(){
+         
+        },
+        stringFinished: function(str){
+          img.prop('src', imgPath + str);
+          //console.log("completed " + imgPath + str)
+        }
+      });
+  });
 
 
 
