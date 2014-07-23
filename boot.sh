839b8396dd6fd82ed6ee81718c003f21913a1d97
Elizabeth Certa <eliz.certa@gmail.com>
2014-07-23 17:11:57 -0400

    Added reminder of port number

--- boot.sh
+++ boot.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # coding: utf-8
-
+STDERR << "Using port 4001\n"
 `bundle exec rerun --dir '.' --pattern '**/*.{rb,slim,js,yml,css,less, scss,sass,erb,html,htmf,haml,ru,haml,md,mdown,markdown}' -- thin start --debug --port=4001`
