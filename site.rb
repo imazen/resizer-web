005cb2a6e05aaf4c1bc0874aa749b66ef4d572a5
Nathanael Jones <nathanael.jones@gmail.com>
2015-04-10 12:57:07 -0400

    Use Hardwired's JsOptimize instead

--- site.rb
+++ site.rb
@@ -129,101 +129,15 @@ class Site < Hardwired::Bootstrap
 
 
     get '/alljs/:scripts' do |scripts|
-      scripts = scripts.split(',').map{|s| Base64.urlsafe_decode64(s)}
-
- 
-
-      compressor = defined?(YUI) && defined?(YUI::JavaScriptCompressor) && YUI::JavaScriptCompressor.new(:munge => false)
-
-      no_minify = dev? 
-
-      compress_callback = lambda do |content, path|
-        begin
-            
-          (compressor ? compressor.compress(content) : content)
-        rescue Exception => e
-          #puts "Syntax error in #{path}"
-          #p e
-          content
-        end
-      end
-
-
-
-      session = Rack::Test::Session.new(Site)
-      combined = scripts.map { |path|
-        content = nil
-        result = session.get(path)
-        if result.body.respond_to?(:force_encoding)
-          response_encoding = result.content_type.split(/;\s*charset\s*=\s*/).last.upcase rescue 'ASCII-8BIT'
-          content = result.body.force_encoding(response_encoding).encode(Encoding.default_external || 'ASCII-8BIT')  if result.status == 200
-        else
-          content = result.body  if result.status == 200
-        end
-
-        (path.end_with?("min.js") || path.end_with?("pack.js") || no_minify) ? content : compress_callback.call(content,path) 
-
-      }.join("\n")
-
+      
       content_type "text/javascript"
       last_modified Time.new(request["m"])
       cache_for 60 * 60 * 24 * 30 #1 month
-      combined
+      Hardwired::JsOptimize.create_combined_response(Site, scripts, dev?)
     end 
 end
 
-module Hardwired
-  class JsOptimize
-    @@include_cache = {}
-    def self.filter_includes(options = {:defer => true}, fragment)
 
-      return @@include_cache[fragment] if @@include_cache[fragment]
-      require 'nokogiri'
-      dom = Nokogiri::HTML::fragment(fragment)
-      scripts = []
-      mod_dates = []
-      dom.css('script').each do |i|
-        url = i["src"]
-        next if url.nil? || url.start_with?("http") || url.start_with?("//")
-        try_urls = [url.sub(/(?<!min)\.js\Z/i,".min.js"),url.sub(/(?<!min)\.js\Z/i,"-min.js"), url]
-        mod_date = nil 
-        try_urls.each do |u|
-          begin 
-            mod_date = File.mtime(Hardwired::Paths.content_path(u))
-            url = u
-            p url if dev?
-            p mod_date if dev?
-            break
-          rescue
-            next
-          end
-        end
-        next if mod_date == nil 
-
-        mod_dates << mod_date
-        scripts << url
-        i.remove
-      end 
-
-      avg_mod_date = mod_dates.map{|d| d.to_f}.reduce(:+).to_f / mod_dates.size
-
-      sNode = Nokogiri::XML::Node.new('script',dom)
-      sNode['defer'] = "defer" if options[:defer]
-      sNode['async'] = "true" if options[:async]
-      sNode['src'] = "/alljs/" + scripts.map{|s| Base64.urlsafe_encode64(s)}.join(',') + "?m=" + Time.at(avg_mod_date).to_s
-
-
-      result = sNode.to_html + dom.to_html
-
-      @@include_cache[fragment] = result
-      result
-
-
-    end 
-
-
-  end
-end 
 
 module Hardwired
   class Template
