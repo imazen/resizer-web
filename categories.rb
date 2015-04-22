b323f1ba7237911adab00dddfebacb846a6024a3
Nathanael Jones <nathanael.jones@gmail.com>
2015-04-22 14:20:17 -0400

    Add page.reachable_in_versions(versions) - checks to see if the current page is reachable under a different version id.

--- categories.rb
+++ categories.rb
@@ -3,6 +3,26 @@ module Hardwired
   class Template
 
 
+    def reachable_in_versions(versions)
+
+      current_version = versions.select{|b| self.path.start_with?(b[:folder])}.first
+
+      if current_version.nil?
+        #STDERR << "#{self.path} not found in versioned folder\n"
+        return [] 
+      end
+
+      versions.map do |hash|
+        prefix = hash[:folder]
+        rel_path = self.path[current_version[:folder].length..-1]
+        other_version = prefix + rel_path
+        dest = Hardwired::Aliases::AliasTable.get_final_destination(other_version, other_version) || other_version
+        final_page = Hardwired::Index[dest]
+        #STDERR << "#{other_version} mapped to #{dest} #{final_page.nil? ? '[missing!]' : '[exists]'}. \n"
+        final_page.nil? ? nil : {active: hash == current_version, page_path: dest}.merge(hash)
+      end.compact
+    end 
+
 
     def after_load
       #Evaluate all categories, assign meta.sidebar and meta.primary_category
