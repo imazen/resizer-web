ede33a400e4bb188b80e02c54ea4538fb8bd0c72
Nathanael Jones <nathanael.jones@gmail.com>
2017-06-27 14:16:07 -0600

    Adjust boot.sh

--- boot.sh
+++ boot.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
 # coding: utf-8
-STDERR << "Using port 4001\n"
-`bundle exec rerun --dir '.' --pattern '**/*.{rb,slim,js,yml,css,less, scss,sass,erb,html,htmf,haml,ru,haml,md,mdown,markdown}' -- bundle exec puma -C puma.rb --debug --port=4001 `
+PORT=4605
+STDOUT << "Running on port #{PORT}\n"
+`bundle exec rerun --dir '.' --pattern '**/*.{rb,slim,js,yml,css,less, scss,sass,erb,html,htmf,haml,ru,haml,md,mdown,markdown}' -- bundle exec puma -C puma.rb --debug --port=#{PORT}`
