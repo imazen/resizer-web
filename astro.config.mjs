bc6b053e43d87db25f496b3983178229abc1d1cf
Lilith River <lilith@imazen.io>
2025-01-30 14:18:16 -0700

    Astro 5 (broken)

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,50 +7,62 @@ import starlightUtils from "@lorenzo_lewis/starlight-utils";
 import redirects from './src/redirects.json';
 import sitemap from "@astrojs/sitemap";
 import redirectFrom from "astro-redirect-from";
+import catppuccin from "starlight-theme-catppuccin";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://imageresizing.net',
   redirects: redirects,
   integrations: [starlight({
+
+    plugins: [
+
+      // starlightUtils({
+      // navLinks: {
+      //   leading: {
+      //     useSidebarLabelled: 'leadingNavLinks'
+      //   }
+      // }}),
+      //  starlightBlog({
+      //   authors: {
+      //     lilith: {
+      //       name: 'Lilith',
+      //       title: 'Lead engineer & owner',
+      //       picture: '/lilith.jpg',
+      //       // Images in the `public` directory are supported.
+      //       url: 'https://github.com/lilith'
+      //     }
+      //   }
+      // }), 
+      //starlightLinksValidator(),
+      // starlightVersions({
+      //   versions: [{ slug: '1-0', label: '1' }],
+      // }), 
+      //sitemap(), 
+      // redirectFrom({
+      //     contentDir: './src/content/docs'
+      //   },
+      // ),
+      //catppuccin(),
+    ],
+    // defaultLocale: 'root',
+    // locales: {
+    //   root: {
+    //     label: 'English',
+    //     lang: 'en' // lang is required for root locales
+    //   },
+    //   'es': {
+    //     label: 'Español',
+    //     lang: 'es'
+    //   }
+    // },
     editLink: {
       baseUrl: 'https://github.com/imazen/resizer-web/edit/astro/'
     },
-    plugins: [starlightBlog(), starlightUtils({
-      navLinks: {
-        leading: {
-          useSidebarLabelled: 'leadingNavLinks'
-        }
-      }
-    }), starlightBlog({
-      authors: {
-        lilith: {
-          name: 'Lilith',
-          title: 'Lead engineer & owner',
-          picture: '/lilith.jpg',
-          // Images in the `public` directory are supported.
-          url: 'https://github.com/lilith'
-        }
-      }
-    }), starlightLinksValidator()
-    // starlightVersions({
-    //   versions: [{ slug: '1-0', label: '1' }],
-    // }),
-    ],
-    defaultLocale: 'root',
-    locales: {
-      root: {
-        label: 'English',
-        lang: 'en' // lang is required for root locales
-      },
-      'es': {
-        label: 'Español',
-        lang: 'es'
-      }
-    },
     title: 'ImageResizer',
     logo: {
-      src: './src/assets/imageresizer_400_00aec4.png',
+      light: './src/assets/imageresizer_400_00aec4.png',
+      dark: './src/assets/imageresizer_400_00aec4.png',
       replacesTitle: true
     },
     social: {
@@ -59,33 +71,31 @@ export default defineConfig({
     sidebar: [{
       label: 'Guides',
       items: [
-      // Each item here is one entry in the navigation menu.
-      {
-        label: 'Example Guide',
-        slug: 'guides/example'
-      }]
-    }, {
-      label: 'Reference',
-      autogenerate: {
-        directory: 'reference'
-      }
-    }, {
-      label: 'leadingNavLinks',
-      items: [{
-        label: 'Start',
-        link: '/start'
-      }, {
-        label: 'Docs',
-        link: '/docs'
-      }, {
-        label: 'Support',
-        link: '/support'
-      }, {
-        label: 'Pricing',
-        link: '/pricing'
-      }]
+        // Each item here is one entry in the navigation menu.
+        {
+          label: 'Example Guide',
+          slug: 'guides/example'
+        }]
+    // }, {
+    //   label: 'Reference',
+    //   autogenerate: {
+    //     directory: 'reference'
+    //   }
+    // }, {
+    //   label: 'leadingNavLinks',
+    //   items: [{
+    //     label: 'Start',
+    //     link: '/start'
+    //   }, {
+    //     label: 'Docs',
+    //     link: '/docs'
+    //   }, {
+    //     label: 'Support',
+    //     link: '/support'
+    //   }, {
+    //     label: 'Pricing',
+    //     link: '/pricing'
+    //   }]
     }]
-  }), sitemap(), redirectFrom({
-	contentDir: './src/content/docs'
   })]
 });
